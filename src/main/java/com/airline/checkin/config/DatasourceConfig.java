85f0787233754eda481f75e352e092ee2cbd1ade
pritvish <pritvish.mandal80@gmail.com>
2021-09-18 00:42:08 +0530

    -> added and removed code for startup #1

--- src/main/java/com/airline/checkin/config/DatasourceConfig.java
+++ src/main/java/com/airline/checkin/config/DatasourceConfig.java
@@ -1,4 +1,34 @@
 package com.airline.checkin.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
 public class DatasourceConfig {
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Bean
+    public DataSource datasource() {
+        return DataSourceBuilder.create()
+                .driverClassName(driverClassName)
+                .url(url)
+                .username(username)
+                .password(password)
+                .build();
+    }
 }
